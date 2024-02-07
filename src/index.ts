91b23514a7a9821c432d90b416ae9a1acc2f7e16
Dorayaki-Chan <eii.pinebook@gmail.com>
2024-02-07 22:59:04 +0900

    Docker+TypeScriptでAlexaにしゃべらすことに成功

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,20 @@
-const str: string = 'Hello, world!';
-console.log(str);
+require('dotenv').config();
+const { exec } = require('child_process');
+
+if (!process.env.AUDIO_PATH) {
+    console.error('ERROR: AUDIO_PATHが設定されていません。');
+    process.exit(1);
+}
+const audioPath:string | undefined = process.env.AUDIO_PATH;
+const command:string = `./src/alexa_remote_control.sh -e speak:"<audio src='${audioPath}'/>こんにちは!DockerプラスTypeScriptからAlexaに話させてみました。"`;
+
+exec(command, (err:string, stdout:string, stderr:string) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+    console.log(`stderr: ${stderr}`);
+});
+
+console.log('end of script');

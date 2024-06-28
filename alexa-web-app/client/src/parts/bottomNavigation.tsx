ba55c4248be6cfb8907cebb01c4fa3276420192c
Dorayaki-Chan <eii.pinebook@gmail.com>
2024-06-29 04:57:20 +0900

    トップ・ボトムバー作成

---
+++ alexa-web-app/client/src/parts/bottomNavigation.tsx
@@ -0,0 +1,60 @@
+import * as React from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import RestoreIcon from '@mui/icons-material/Restore';
+import SettingsIcon from '@mui/icons-material/Settings';
+import Paper from '@mui/material/Paper';
+import HomeIcon from '@mui/icons-material/Home';
+
+
+const pathToIndex: Record<string, number> = {
+    '/config': 0,
+    '/': 1,
+    '/log': 2
+};
+
+function BottomBar() {
+    const [value, setValue] = React.useState(0);
+    const ref = React.useRef<HTMLDivElement>(null);
+    const location = useLocation();
+    const navigate = useNavigate();
+
+    
+    React.useEffect(() => {
+        // location.pathnameがpathToIndexに存在するかチェックし、存在しない場合はデフォルト値0を使用
+        const newValue = pathToIndex[location.pathname] !== undefined ? pathToIndex[location.pathname] : 4;
+        setValue(newValue);
+    }, [location.pathname]); // location.pathnameが変更されたときのみsetValueを実行
+
+    const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+        setValue(newValue);
+        // ボトムバーの値に応じてページを切り替える
+         // インデックスに基づいてパスを逆引きする
+        const path = Object.keys(pathToIndex).find(key => pathToIndex[key] === newValue);
+        if (path) {
+            // ナビゲーションロジックをここに実装
+            // 例: history.push(path); // react-router-domの場合
+            console.log(path);
+            navigate(path);
+        }
+    }
+
+    return (
+        <Box sx={{ pb: 7  }} ref={ref}>
+        <CssBaseline />
+            <Paper sx={{position: 'fixed', bottom: 0, left: 0, right: 0}} elevation={3}>
+                <BottomNavigation showLabels value={value} onChange={(event, newValue) => { handleChange(event, newValue) }}>
+                    <BottomNavigationAction label="設定" icon={<SettingsIcon />} />
+                    <BottomNavigationAction label="ホーム" icon={<HomeIcon />} />
+                    <BottomNavigationAction label="ログ" icon={<RestoreIcon />} />
+                </BottomNavigation>
+            </Paper>
+        </Box>
+    );
+}
+
+
+export default BottomBar;
---
+++ alexa-web-app/client/src/parts/topNavigation.tsx
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import CssBaseline from '@mui/material/CssBaseline';
+import useScrollTrigger from '@mui/material/useScrollTrigger';
+
+import TopBarImg from '../assets/img/F_bb.png';
+import Logo from '../assets/img/my-site-logo.png';
+
+
+function TopBar() {
+    const [isMainTopBarVisible, setIsMainTopBarVisible] = useState(true);
+
+    const trigger = useScrollTrigger({
+        disableHysteresis: true,
+        threshold: 0,
+    });
+
+    useEffect(() => {
+        setIsMainTopBarVisible(!trigger);
+    }, [trigger]);
+
+    return (
+        <div>
+        <CssBaseline />
+        <AppBar 
+            position= 'fixed'
+            sx={{ 
+                top: isMainTopBarVisible ? 0 : -56,
+                transition: 'top 0.3s ease-in-out',
+                height: '56px',
+                backgroundColor: 'secondary.contrastText'
+            }}>
+            <Toolbar sx={{ justifyContent: 'center' }}>
+                <img src={`${Logo}`} alt="" width="56" height="56" />
+                <Typography variant="h5" component="div" sx={{color:'text.primary' }}>
+                    Alexa情報統合システム
+                </Typography>
+            </Toolbar>
+        </AppBar>
+        <AppBar 
+            position= 'fixed'
+            sx={{ 
+                top: isMainTopBarVisible ? 56 : 0,
+                transition: 'top 0.3s ease-in-out',
+                height: '56px',
+                backgroundImage: `url(${TopBarImg})`,
+                backgroundSize: '100% 100%'
+            }}>
+            <Toolbar sx={{ justifyContent: 'center' }}>
+                <Typography variant="h4" component="div">
+                    ホーム
+                </Typography>
+            </Toolbar>
+        </AppBar>
+        </div>
+    );
+}
+
+export default TopBar;

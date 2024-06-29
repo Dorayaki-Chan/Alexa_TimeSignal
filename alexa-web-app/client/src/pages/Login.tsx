39127dba75f956279b509f2e856516c94f97b6f9
Dorayaki-Chan <eii.pinebook@gmail.com>
2024-06-30 06:05:56 +0900

    ログインアイコン削除

--- alexa-web-app/client/src/pages/Login.tsx
+++ alexa-web-app/client/src/pages/Login.tsx
@@ -144,18 +144,18 @@ function SignIn() {
 			<CssBaseline />
 			<Box
 			sx={{
-				marginTop: 8,
+				marginTop: 16,
 				display: 'flex',
 				flexDirection: 'column',
 				alignItems: 'center',
 			}}
 			>
-				<Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+				{/* <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
 					<LockOutlinedIcon />
 				</Avatar>
 				<Typography component="h1" variant="h5">
 					ログイン
-				</Typography>
+				</Typography> */}
 				<Box component="form" onSubmit={(e) => handleSubmit(e)} noValidate sx={{ mt: 1 }}>
 					<TextField
 					margin="normal"

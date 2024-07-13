a7ca8f9f48cd0ba08de7406776b39a2715dc92ac
Dorayaki-Chan <eii.pinebook@gmail.com>
2024-07-14 04:41:27 +0900

    console.logを加筆することで謎エラー回避

--- alexa-web-app/client/src/parts/contexts.ts
+++ alexa-web-app/client/src/parts/contexts.ts
@@ -2,12 +2,16 @@ import { createContext } from "react";
 
 const PageNameContext = createContext({
     pageName: '',
-    setPageName: (name: string) => {},
+    setPageName: (name: string) => {
+        console.log(name);
+    },
 });
 
 const TopBarImgContext = createContext({
     topBarImg: '',
-    setTopBarImg: (topBarImg: string) => {},
+    setTopBarImg: (topBarImg: string) => {
+        console.log(topBarImg);
+    },
 });
 
 export { PageNameContext, TopBarImgContext };
